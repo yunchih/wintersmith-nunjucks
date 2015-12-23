7cdbeae903f19569be64f0a2e367f5f4d00b3f18
IthacaCow <cba840910@gmail.com>
2015-12-23 15:19:46 +0800

    add extension

--- index.js
+++ index.js
@@ -10,19 +10,30 @@ module.exports = function(env, callback) {
   var loader = new nunjucks.FileSystemLoader(env.templatesPath, loaderOpts);
   var nenv = new nunjucks.Environment(loader);
 
+  // Configure nunjucks environment.
+  if (env.config.nunjucks) {
+    nenv.opts = env.config.nunjucks.opts | {
+        autoescape: true    
+    };
+  }
+
   // Load the filters
   if(env.config.nunjucks && env.config.nunjucks.filterdir) {
     env.config.nunjucks.filters.map( function (name) {
-      file = path.join(env.config.nunjucks.filterdir, name + ".js");
+      file = path.join(env.config.nunjucks.filters_dir, name + ".js");
       filter = env.loadModule(env.resolvePath(file), true);
       nenv.addFilter(name, filter);
     });
   }
 
-  // Configure nunjucks environment.
-  if (env.config.nunjucks && env.config.nunjucks.autoescape != null) {
-    nenv.opts.autoescape = env.config.nunjucks.autoescape;
+  if(env.config.nunjucks && env.config.nunjucks.extensions) {
+    env.config.nunjucks.extensions.map( function (name) {
+      file = path.join(env.config.nunjucks.extensions_dir, name + ".js");
+      extension = env.loadModule(env.resolvePath(file), true);
+      nenv.addExtension(name, extension);
+    });
   }
+
 
   var NunjucksTemplate = function(template) {
     this.template = template;
