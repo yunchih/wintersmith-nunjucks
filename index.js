1d820e06d5e7d329fe64a598572c8b0d4c946adf
IthacaCow <cba840910@gmail.com>
2015-12-25 10:13:31 +0800

    revert back to original setting plus extra extensions

--- index.js
+++ index.js
@@ -4,31 +4,20 @@ var path = require("path");
 module.exports = function(env, callback) {
 
   // Load the new nunjucks environment.
-  var loaderOpts = {
-    watch: (env.mode == 'preview')
-  };
-  var loader = new nunjucks.FileSystemLoader(env.templatesPath, loaderOpts);
-  var nenv = new nunjucks.Environment(loader);
-
-  // Configure nunjucks environment.
-  if (env.config.nunjucks) {
-    nenv.opts = env.config.nunjucks.opts | {
-        autoescape: true    
-    };
-  }
+  var nenv = new nunjucks.Environment(new nunjucks.FileSystemLoader(env.templatesPath));
 
   // Load the filters
-  if(env.config.nunjucks && env.config.nunjucks.filters_dir) {
+  if(env.config.nunjucks && env.config.nunjucks.filterdir) {
     env.config.nunjucks.filters.map( function (name) {
-      file = path.join(env.config.nunjucks.filters_dir, name + ".js");
+      file = path.join(env.config.nunjucks.filterdir, name + ".js");
       filter = env.loadModule(env.resolvePath(file), true);
       nenv.addFilter(name, filter);
     });
   }
 
-  if(env.config.nunjucks && env.config.nunjucks.extensions) {
+  if(env.config.nunjucks && env.config.nunjucks.extension_dir) {
     env.config.nunjucks.extensions.map( function (name) {
-      file = path.join(env.config.nunjucks.extensions_dir, name + ".js");
+      file = path.join(env.config.nunjucks.extension_dir, name + ".js");
       extension = env.loadModule(env.resolvePath(file), true);
       nenv.addExtension(name, extension);
     });
